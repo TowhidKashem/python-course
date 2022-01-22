248e762203a7734c823e8f640b03b0e31843a228
Towhid Kashem <tkashem@gmail.com>
2022-01-21 22:12:07 -0600

    work

--- dictionaries.py
+++ dictionaries.py
@@ -8,6 +8,7 @@ car = {
 }
 
 car.get('model')  # Mustang
+car['model']  # Mustang
 
 car["brand"] = "Mercedes"  # update or add new values
 
@@ -15,8 +16,13 @@ car.pop('model')  # remove the model key/value pair
 
 car.clear()  # empty the dictionary "{}"
 
+car.copy()  # make a copy, same as "newCar = { ...car }"
+
 # convert a list of tuples into a dictionary using dict comprehensions
 stats = [('age', 22), ('height', 5.9), ('weight', 170)]
 dict_stats = {key: value for (key, value) in stats}
 
 print(dict_stats)  # {'age': 22, 'height': 5.9, 'weight': 170}
+
+
+del car['brand']  # delete by key
--- lists.py
+++ lists.py
@@ -6,9 +6,16 @@ myList[0]  # 1
 
 myList[-1]  # 'hello', same as myList[myList.length - 1]
 
+myList[0:2]  # [1, 2], same as myList.slice(0, 2)
+
 myList[0] + 1  # 2, but original list value is not mutated
 
 myList.append('world')  # same as push()
+
+# add to the end of a list, same as using spread operator
+my_list = [1, 2, 3]
+my_list.extend([4, 5, 6])  # [1, 2, 3, 4, 5, 6]
+
 
 myList.pop()  # 'world', myList is mutated
 
@@ -16,6 +23,8 @@ myList.pop()  # 'world', myList is mutated
 myList.insert(1, 'wow')
 
 myList.remove('inserted')  # same as filter()'ing by value
+
+del myList[0]  # delete by index
 
 
 # list comprehensions, same as <array>.map(num => num * 2)
@@ -25,3 +34,17 @@ doubled_nums = [num * 2 for num in nums]  # [2, 4, 6]
 # conditionals are also supported when added to the end
 # double only the number "2" and return it as a new list
 doubled_nums = [num * 2 for num in nums if num == 2]  # [4]
+
+
+# make a shallow copy of the list (works on tuples as well)
+# since it's shallow if the list had something like an object it will be a reference to it
+copy = myList[:]
+
+
+myList = [True, False, True]
+any(myList)  # True
+all(myList)  # False
+
+# are all numbers greater than 0
+myList = [1, 2, 3, -5]
+all([el > 0 for el in myList])  # False
